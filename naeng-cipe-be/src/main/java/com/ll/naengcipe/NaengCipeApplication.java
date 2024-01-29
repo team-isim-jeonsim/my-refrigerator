9d294e7e12777bad55cd6b982dce8e7649bcc32d
seo-sang-ho <125337743+seo-sang-ho@users.noreply.github.com>
2024-01-29 17:09:51 +0900

    [#8,9,10] 나의 냉장고 기능 구현 (#27)
    * 충동 해결
    
    * 충돌 해결
    
    * [#8,9,10] feat: 나의 냉장고 관련 기능 추가
    
    * style: 코딩 컨벤션 스타일에 맞춰 수정
    
    * refactor: Optional의 get()사용 지양, 예외처리

--- naeng-cipe-be/src/main/java/com/ll/naengcipe/NaengCipeApplication.java
+++ naeng-cipe-be/src/main/java/com/ll/naengcipe/NaengCipeApplication.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class NaengCipeApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(NaengCipeApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(NaengCipeApplication.class, args);
+	}
 }
