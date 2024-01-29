aa7512d46be16a4e3ced5047b8a4bdb0b65aa4e1
seo-sang-ho <rocter2049@naver.com>
2024-01-29 13:54:07 +0900

    refactor: Optional의 get()사용 지양, 예외처리

--- naeng-cipe-be/src/main/java/com/ll/naengcipe/domain/fridge/fridge/service/FridgeService.java
+++ naeng-cipe-be/src/main/java/com/ll/naengcipe/domain/fridge/fridge/service/FridgeService.java
@@ -1,18 +1,16 @@
 package com.ll.naengcipe.domain.fridge.fridge.service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ll.naengcipe.domain.fridge.fridge.entity.Fridge;
-import com.ll.naengcipe.domain.fridge.fridge.entity.FridgeIngredient;
 import com.ll.naengcipe.domain.fridge.fridge.repository.FridgeIngredientRepository;
 import com.ll.naengcipe.domain.fridge.fridge.repository.FridgeRepository;
 import com.ll.naengcipe.domain.ingredient.ingredient.entity.Ingredient;
 import com.ll.naengcipe.domain.ingredient.ingredient.repository.IngredientRepository;
-import com.ll.naengcipe.domain.member.member.entity.Member;
 import com.ll.naengcipe.domain.recipe.recipe.dto.RecipeResponseDto;
 import com.ll.naengcipe.domain.recipe.recipe.repository.RecipeRepository;
 
@@ -32,7 +30,7 @@ public class FridgeService {
 		return recipeRepository.findRecipesByIngredients(ingredients);
 	}
 
-	public Fridge findByUserId(Long id){
+	public Fridge findByUserId(Long id) {
 		return fridgeRepository.findByMemberId(id);
 	}
 
@@ -41,16 +39,13 @@ public class FridgeService {
 	}
 
 	@Transactional
-	public void addIngredient(Long ingredientId,Long userId) {
+	public void addIngredient(Long ingredientId, Long userId) {
 		// 재료 찾기
-		Ingredient ingredient = ingredientRepository.findById(ingredientId).get();
+		Ingredient ingredient = ingredientRepository.findById(ingredientId)
+			.orElseThrow(() -> new NoSuchElementException("해당 재료가 존재하지 않습니다."));
 
 		// 로그인 된 아이디의 냉장고 찾기
 		Fridge userFridge = findByUserId(userId);
-
-		if(userFridge == null){
-
-		}
 
 		userFridge.addIngredient(ingredient);
 	}
@@ -58,7 +53,8 @@ public class FridgeService {
 	@Transactional
 	public void removeIngredient(Long ingredientId, Long userId) {
 		// 재료 찾기
-		Ingredient ingredient = ingredientRepository.findById(ingredientId).get();
+		Ingredient ingredient = ingredientRepository.findById(ingredientId)
+			.orElseThrow(() -> new NoSuchElementException("해당 재료가 존재하지 않습니다."));
 
 		// 로그인 된 아이디의 냉장고 찾기
 		Fridge userFridge = findByUserId(userId);
